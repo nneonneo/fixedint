b47d03e620118e18a55c91676ad3bcd780ac08d9
Robert Xiao <brx@cs.cmu.edu>
2014-06-21 22:08:29 -0400

    Remove local testing script

--- test.sh
+++
@@ -1,7 +0,0 @@
-python2.5 -m fixedint.test_fixedint
-python2.6 -m fixedint.test_fixedint
-python2.7 -m fixedint.test_fixedint
-python3.2 -m fixedint.test_fixedint
-python3.4 -m fixedint.test_fixedint
-jython -m fixedint.test_fixedint
-pypy -m fixedint.test_fixedint
